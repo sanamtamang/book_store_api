aca770ec0c3ee3e18a3d399b0daf1911438755d5
Sanam Tamang <lamasanam2017@gmail.com>
2022-07-29 21:14:41 +0545

    update api

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-require("dotenv").config();
-
 const express = require("express");
-const { Client } = require("pg");
-
-//Express App
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
 
 const app = express();
 
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
 
-client.connect((err) => {
-  if (err) {
-    console.log("Database Connection Failed");
-  } else {
-    console.log("Database Connection Successfully");
-  }
-});
 app.get("/book/list", async (req, res) => {
-  const results = await client.query("select * from book");
+  const results = await pool.query("select * from book");
   const books = results.rows.map((book, i) => {
     return {
       id: book.id,
       title: book.title,
       author: book.author,
+      imageUrl: book.imageurl,
       description: {
         publish_date: book.publish_date,
         price: book.price,
@@ -36,6 +30,60 @@ app.get("/book/list", async (req, res) => {
     };
   });
   res.json(books);
+});
+
+app.get("/users/login", (req, res) => {
+  res.send("login");
+});
+
+app.post("/users/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  let errors = [];
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Password should be at least 6 characters" });
+  }
+
+  if (password !== password2) {
+    errors.push({ message: "Passwords  do not match" });
+  }
+
+  if (errors.length < 6) {
+    res.render("register", { errors });
+  } else {
+    let hashedPassword = await bcrypt.hash(password, 10);
+    pool.query(
+      `SELECT * FROM users WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          throw err;
+        } else {
+          pool.query(
+            `INSERT INTO users (name ,email,password) VALUES ($1,$2,$3) RETURNING id, password`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              req.flash("success_msg", "you are now registered . please login");
+              res.redirect("/users/login");
+              res.json(results.rows);
+            }
+          );
+        }
+
+        if (results.rows.length > 0) {
+          errors.push({ message: "Email already registered" });
+          res.render("register", { errors });
+        }
+      }
+    );
+  }
 });
 
 app.listen(process.env.PORT, () => {
